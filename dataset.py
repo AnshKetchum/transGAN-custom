fab07a1876820774c71c4973cbf582beca56a0c8
Ansh <achaurasia@ocf.berkeley.edu>
2022-09-14 15:47:04 -0700

    First push

---
+++ dataset.py
@@ -0,0 +1,43 @@
+'''
+This cell contains the Dataset object 
+for the Pytorch Dataloader API 
+'''
+
+import torch
+import PIL
+import pandas as pd
+from torchvisions import transforms
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, IMAGE_SIZE = 256):
+
+        #Initialize parameters from constructor
+        self.df = pd.read_csv(os.path.join(YOLO_DIR, 'raw_dataset' ,'images-info.csv'))
+        self.transforms = transforms.Compose( 
+            [
+            transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+            transforms.ToTensor(), #Convert to tensor
+            transforms.Normalize( mean=mean , std= std)] #Normalize - subtract by the mean and divide by the standard deviation
+        )
+
+
+    #Returns length of the dataset
+    def __len__(self):
+        return len(self.df)
+
+    #Gets a specific item from the dataset
+    def __getitem__(self, index):
+
+        #Get the ith row of the CSV - has all the information
+        row = self.df.iloc[index]
+
+        #Read the image
+        X = PIL.Image.open(os.path.join(YOLO_DIR, 'raw_dataset', row["dataset-path"])).convert("RGB")
+        X = self.transforms(X)
+
+        #Read the labels
+        y = row["label"]
+
+        return X,y
+
+print("Dataset class compiled.")
