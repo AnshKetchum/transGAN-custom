36db0a7cf0bfb9caf3d46c6a169c335341632101
Ansh <achaurasia@ocf.berkeley.edu>
2022-09-20 21:23:36 -0700

    Convert images to tensors during loading

--- fid_score.py
+++ fid_score.py
@@ -163,7 +163,7 @@ def get_activations(gen_imgs, model, batch_size=50, dims=2048,
         print(type(images), type(gen_imgs))
 
         if type(images) == np.ndarray:
-            images = torch.from_numpy(images)
+            images = torch.from_numpy(np.vstack(images).astype(np.float))
 
         model.to("cuda:0")
         pred = model(images.to("cuda:0"))[0]
